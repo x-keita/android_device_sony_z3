8156d757f1abe6feedb8b53297a97144e2ad8897
Keita <keita_nya@live.jp>
2017-10-23 04:00:59 -0300

    z3: Use .291 device build fingerprint
    * Fixes SafetyNet CTS mismatch.

--- lineage.mk
+++ lineage.mk
@@ -3,9 +3,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/leo/leo:N/NPD56N/1000106:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="leo-user N NPD56N 1000106 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES +=  \
+	PRODUCT_NAME=D6603 \
+	BUILD_FINGERPRINT=Sony/D6603/D6603:6.0.1/23.5.A.1.291/2769308465:user/release-keys \
+	PRIVATE_BUILD_DESC="D6603-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
 
 PRODUCT_NAME := lineage_z3
 PRODUCT_DEVICE := z3
