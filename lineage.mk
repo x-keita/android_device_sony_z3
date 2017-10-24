8445cd9a1c76db0b4e7f07346768ec6f16712112
Keita <keita_nya@live.jp>
2017-10-24 02:08:18 -0300

    z3: vendor/cm -> vendor/lineage

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
